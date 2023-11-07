a6810a88ecbfbd525bbb83876aadc60f79d570a7
Keenan Graham <ksgraham@stanford.edu>
2023-11-07 13:29:31 -0800

    Add signature verification and SQS/secret

--- app.py
+++ app.py
@@ -9,11 +9,16 @@ from aws_cdk.aws_lambda import FunctionUrlAuthType
 
 from aws_cdk.aws_lambda_python_alpha import PythonFunction
 
+from aws_cdk.aws_secretsmanager import Secret
+
+from aws_cdk.aws_sqs import DeadLetterQueue
+from aws_cdk.aws_sqs import Queue
+
 from constructs import Construct
 
 
 US_WEST_2 = Environment(
-    account='618537831167',
+    account='109189702753',
     region='us-west-2',
 )
 
@@ -23,16 +28,50 @@ class GithubWebhook(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        python_function = PythonFunction(
+        secret = Secret.from_secret_complete_arn(
             self,
-            'PythonFunction',
+            'Secret',
+            secret_arn='arn:aws:secretsmanager:us-west-2:109189702753:secret:github-webhook-secret-hz6JXf',
+        )
+
+        dead_letter_queue = Queue(
+            self,
+            'DeadLetterQueue',
+            retention_period=Duration.days(14),
+        )
+
+        queue = Queue(
+            self,
+            'Queue',
+            visibility_timeout=Duration.seconds(120),
+            dead_letter_queue=DeadLetterQueue(
+                queue=dead_letter_queue,
+                max_receive_count=3,
+            )
+        )
+
+        handler = PythonFunction(
+            self,
+            'Handler',
             runtime=Runtime.PYTHON_3_10,
             entry='lambda',
             memory_size=512,
             timeout=Duration.seconds(60),
+            environment={
+                'QUEUE_URL': queue.queue_url,
+                'SECRET_ARN': secret.secret_arn,
+            }
         )
 
-        function_url = python_function.add_function_url(
+        secret.grant_read(
+           handler
+        )
+
+        queue.grant_send_messages(
+            handler
+        )
+
+        function_url = handler.add_function_url(
             auth_type=FunctionUrlAuthType.NONE,
         )
 
@@ -45,7 +84,8 @@ class GithubWebhook(Stack):
 
 app = App()
 
-GithubWebhook
+
+GithubWebhook(
     app,
     'GithubWebhook',
     env=US_WEST_2,
--- lambda/index.py
+++ lambda/index.py
@@ -1,5 +1,85 @@
+import hmac
+
+import hashlib
+
+import json
+
+import os
+
+import boto3
+
+from botocore.exceptions import ClientError
+
+
+sm_client = boto3.client('secretsmanager')
+
+sqs_client = boto3.client('sqs')
+
+QUEUE_URL = os.environ['QUEUE_URL']
+
+
+def get_github_secret():
+    secret_arn = os.environ['SECRET_ARN']
+    return json.loads(sm_client.get_secret_value(SecretId=secret_arn)['SecretString'])['SECRET']
+
+
+def verify_hmac(secret, body, signature):
+    hmac_gen = hmac.new(secret.encode(), body.encode(), hashlib.sha256).hexdigest()
+
+    expected_signature = f'sha256={hmac_gen}'
+
+    return hmac.compare_digest(expected_signature, signature)
+
+
 def handler(event, context):
     print(event)
+
+    github_secret = get_github_secret()
+
+    github_signature = event['headers'].get('x-hub-signature-256')
+
+    if not github_signature:
+        return {
+            'statusCode': 401,
+            'body': json.dumps(
+                'Unauthorized - Missing GitHub Signature'
+            )
+        }
+
+    payload_body = event['body']
+
+    github_event = event['headers'].get('x-github-event', '')
+
+    if github_event != 'delete':
+        return {
+            'statusCode': 202,
+            'body': json.dumps(f'Ignored {github_event} event')
+        }
+    
+    signature_valid = verify_hmac(github_secret, payload_body, github_signature)
+
+    if not signature_valid:
+        return {
+            'statusCode': 403,
+            'body': json.dumps('Unauthorized - Invalid signature')
+        }
+
+    payload = json.loads(request_body)
+
+    if payload.get('action') == 'deleted':
+        try:
+            sqs_response = sqs_client.send_message(
+                QueueUrl=QUEUE_URL,
+                MessageBody=request_body
+            )
+        except ClientError as e:
+            print(f'Error sending message to SQS: {e}')
+            return {
+                'statusCode': 500,
+                'body': json.dumps({'message': 'Internal server error'})
+            }
+
     return {
-        'message': 'ok'
+        'statusCode': 200,
+        'body': json.dumps('Delete event processed successfully')
     }
